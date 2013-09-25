159c741c9413b059a424d8e366ca69f879901b25
Matt Boersma <matt@opdemand.com>
2013-09-25 09:17:53 -0600

    More updates

--- contrib/provision-rackspace-controller.sh
+++ contrib/provision-rackspace-controller.sh
@@ -5,6 +5,44 @@ if [ -z $1 ]; then
   exit 1
 fi
 
+<<<<<<< HEAD
+# check for Deis' general dependencies
+thisdir=`dirname $0`
+if ! $thisdir/check-deis-deps.sh; then
+  echo 'Deis is missing some dependencies.'
+  exit 1
+fi
+
+# check for knife-rackspace
+if ! knife rackspace server list > /dev/null; then
+  echo 'Please install the knife-rackspace Ruby gem and configure knife.rb.'
+  exit 1
+fi
+
+region=$1
+
+# see contrib/prepare-rackspace-image.sh for instructions
+# on creating your own deis-optmized images
+if ! [[ "ord dfw iad lon syd" =~ $region ]]; then
+  echo "Unrecognized region: $region"
+  exit 1
+fi
+
+# rackspace settings
+flavor=$(knife rackspace flavor list | grep '2GB Standard Instance' | awk '{print $1}')
+image=$(knife rackspace image list --rackspace-region $region | grep 'deis-base-image' | awk '{print $1}')
+if [[ -z $image ]]; then
+  echo 'Please create "deis-base-image" using the prepare-rackspace-image.sh script.'
+  exit 1
+fi
+# sg_name=deis-controller
+# sg_src=0.0.0.0/0
+key_name=deis-controller
+
+# ssh settings
+ssh_key_path=~/.ssh/$key_name
+ssh_user="ubuntu"
+=======
 region=$1
 
 # TODO: prepare an optimized snapshot for this account
@@ -18,6 +56,7 @@ image="23b564c9-c3e6-49f9-bc68-86c7a9ab5018"
 # ssh settings
 # ssh_key_path=~/.ssh/$key_name
 ssh_user="root"
+>>>>>>> 10b9a31f235aae735afe7949811a129f8d5e6dfe
 
 # chef settings
 node_name="deis-controller"
@@ -58,6 +97,31 @@ function echo_color {
 # fi
 
 # create data bags
+<<<<<<< HEAD
+knife data bag create deis-users 2>/dev/null
+knife data bag create deis-formations 2>/dev/null
+knife data bag create deis-apps 2>/dev/null
+
+# trigger Rackspace instance bootstrap
+echo_color "Provisioning $node_name with knife rackspace..."
+set -x
+knife rackspace server create \
+ --bootstrap-version $chef_version \
+ --rackspace-region $region \
+ --image $image \
+ --flavor $flavor \
+ --server-name $node_name \
+ --ssh-user $ssh_user \
+ --identity-file $ssh_key_path \
+ --node-name $node_name \
+ --run-list $run_list \
+ --server-create-timeout 900
+ # --rackspace-metadata '{"Name": "$node_name"}' \
+ # --groups $sg_name \
+ # --tags Name=$node_name \
+ # --ssh-key $key_name \
+set +x
+=======
 knife data bag create deis-build 2>/dev/null
 knife data bag create deis-formations 2>/dev/null
 
@@ -91,3 +155,4 @@ knife
 #  --ebs-size $ebs_size \
 #  --run-list $run_list
 # set +x
+>>>>>>> 10b9a31f235aae735afe7949811a129f8d5e6dfe
